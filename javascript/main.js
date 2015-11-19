b9c3abae25390bd09339e9ba1ad63a8322b3ae89
azeneli <azeneli46@gmail.com>
2015-11-19 16:43:45 -0500

    Draggable func implemented

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,20 @@
 /* CURRENTLY IN: javascript/main.js */
+
+(function(){ // protect da scope
+
+	$('.js-select').draggable({
+		containment: 'window',
+		stack: '.block',
+		snap: true,
+		snapMode: 'outer',
+		snapTolerance: 13,
+	});
+	
+
+
+	$('.blocks__canvas').on('mouseover', function () {
+		$('.block__canvas-title').fadeOut('slow');
+	});
+
+
+})();
